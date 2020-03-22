98aed2d099092cf4bcbab8723387ba332f4c5f74
MashinaSSokom <skltan@mail.ru>
2020-03-22 10:09:21 +0800

    Revert "Merge branch 'master' of https://github.com/NaniB0ots/Smart-Emoji"
    This reverts commit abaf1833f122f898253ee29faf3e4374471c675b, reversing
    changes made to 55a8aa0a1e7e8004b4d21db2a8175e47a216404d.

--- SmartEmoji/urls.py
+++ SmartEmoji/urls.py
@@ -4,6 +4,4 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('choose_emoji', views.choose_emoji, name='choose_emoji'),
-    path('emoji_books', views.emoji_books, name='emoji_books'),
 ]
--- app/BD.py
+++ app/BD.py
@@ -1,26 +1,17 @@
 import sqlite3
-import os
 
-# __connection = None
+__connection = None
 # Подключение
-# def get_connection():
-#     global __connection
-#     if __connection is None:
-#         Base_DIR = os.path.dirname(__file__)
-#         __connection = sqlite3.connect(Base_DIR + '/books.db', check_same_thread=False)
-#         print("Connected to BD")
-#     return __connection
-def conection():
-    Base_DIR = os.path.dirname(__file__)
-    global conn
-    conn = sqlite3.connect(Base_DIR + '/books.db', check_same_thread=False)
-    return conn
-
-def get_conn():
-    return conn
+def get_connection():
+    global __connection
+    if __connection is None:
+        __connection = sqlite3.connect('books.db')
+        print("Connected to BD")
+    return __connection
 
 # Инициализация ДБ
 def init_db():
+    conn = get_connection()
     c = conn.cursor()
     c.execute('PRAGMA foreign_keys=on')
 
@@ -41,74 +32,70 @@ def init_db():
     conn.commit()
     c.close()
 
-
-def insert_book(book_name, author, style, count, price, img, emoji, page_count, id_book=None):
+def insert_book(book_name,  author, style, count, price, img, emoji, page_count, id_book = None ):
+    conn = get_connection()
     c = conn.cursor()
-    temp = (id_book, book_name, author, style, count, price, img, emoji, page_count)
+    temp = (id_book, book_name,  author, style, count, price, img, emoji, page_count)
     c.executemany('INSERT INTO books VALUES(?,?,?,?,?,?,?,?,?)', (temp,))
     conn.commit()
     print('books updated')
     c.close()
 
-
-# init_db()
-# insert_book('Книга_1', 'Автор_1', 'Любовынй роман', 4, 100, 'Book.png', 'Романтичные,Сексуальные,Трогательные', 888)
-# insert_book('Книга_2', 'Автор_2', 'Фантастика', 5, 150, 'Book.png', 'Мистика,Экшн,Волнующие,Странные', 162)
-# insert_book('Книга_3', 'Автор_3', 'Здоровье.Фитнес.Спорт', 6, 120, 'Book.png', 'Вдохновение,Молодежный', 362)
-# insert_book('Книга_4', 'Автор_4', 'Фантастика', 7, 110, 'Book.png', 'Мистика,Научные,Экшн,Жестокие', 58)
-# insert_book('Книга_5', 'Автор_1', 'Комиксы', 8, 200, 'Book.png', 'Смешные,Странные,Волнующие,Молодежные,Трогательные', 35)
-# insert_book('Книга_6', 'Автор_2', 'Комиксы', 9, 250, 'Book.png', 'Жестокие,Страшные', 20)
-# insert_book('Книга_7', 'Автор_3', 'Биография', 8, 100, 'Book.png', 'Вдохновение,Научные,Добрые', 97)
-# insert_book('Книга_8', 'Автор_4', 'Детектив', 7, 130, 'Book.png', 'Странные,Мистика,Умные,Волнующие', 105)
-# insert_book('Книга_9', 'Автор_1', 'Детектив', 6, 150, 'Book.png', 'Жестокие,Трогательные,Волнующие,Экшн', 85)
-# insert_book('Книга_10', 'Автор_2', 'Энциклопедия', 5, 140, 'Book.png', 'Умные,Научные,Молодежные', 5000)
+#init_db()
+#insert_book('Книга_1', 'Автор_1', 'Любовынй роман', 4, 100, 'Book.png', 'Романтичные,Сексуальные,Трогательные', 888)
+#insert_book('Книга_2', 'Автор_2', 'Фантастика', 5, 150, 'Book.png', 'Мистика,Экшн,Волнующие,Странные', 162)
+#insert_book('Книга_3', 'Автор_3', 'Здоровье.Фитнес.Спорт', 6, 120, 'Book.png', 'Вдохновение,Молодежный', 362)
+#insert_book('Книга_4', 'Автор_4', 'Фантастика', 7, 110, 'Book.png', 'Мистика,Научные,Экшн,Жестокие', 58)
+#insert_book('Книга_5', 'Автор_1', 'Комиксы', 8, 200, 'Book.png', 'Смешные,Странные,Волнующие,Молодежные,Трогательные', 35)
+#insert_book('Книга_6', 'Автор_2', 'Комиксы', 9, 250, 'Book.png', 'Жестокие,Страшные', 20)
+#insert_book('Книга_7', 'Автор_3', 'Биография', 8, 100, 'Book.png', 'Вдохновение,Научные,Добрые', 97)
+#insert_book('Книга_8', 'Автор_4', 'Детектив', 7, 130, 'Book.png', 'Странные,Мистика,Умные,Волнующие', 105)
+#insert_book('Книга_9', 'Автор_1', 'Детектив', 6, 150, 'Book.png', 'Жестокие,Трогательные,Волнующие,Экшн', 85)
+#insert_book('Книга_10', 'Автор_2', 'Энциклопедия', 5, 140, 'Book.png', 'Умные,Научные,Молодежные', 5000)
 
 
 def search_by_author(author):
-    conn()
+    conn = get_connection()
     c = conn.cursor()
     temp = []
     c.execute('''SELECT * FROM books
-                 WHERE author = (?)''', (author,))
+                 WHERE author = (?)''',(author,))
     all_book = c.fetchall()
     print(len(all_book))
     l = len(all_book)
     for i in range(l):
         print(all_book[i][1])
-        temp.append(
-            {'book_name': all_book[i][1], 'author': all_book[i][2], 'style': all_book[i][3], 'count': all_book[i][4],
-             'price': all_book[i][5], 'img': all_book[i][6], 'emoji': all_book[i][7], 'page_count': all_book[i][8]})
+        temp.append({'book_name':all_book[i][1],'author': all_book[i][2],'style':all_book[i][3],'count':all_book[i][4],'price':all_book[i][5],'img':all_book[i][6],'emoji':all_book[i][7],'page_count':all_book[i][8]})
     print(temp)
     return temp
 
-
 def search_by_emoji(emoji):
-    conection()
     temp = []
+    conn = get_connection()
     c = conn.cursor()
     c.execute('''SELECT id_book FROM books''')
     l = len(c.fetchall())
     for i in range(l):
         c.execute('''SELECT emoji FROM books
-                     WHERE id_book = (?)''', (i + 1,))
+                     WHERE id_book = (?)''',(i+1,))
         temp_emoji = "".join(c.fetchone())
         if emoji in temp_emoji:
             c.execute(''' SELECT * FROM books
-                          WHERE id_book = (?)''', (i + 1,))
+                          WHERE id_book = (?)''',(i+1,))
             book = c.fetchone()
             temp.append({'book_name': book[1], 'author': book[2], 'style': book[3],
-                         'count': book[4], 'price': book[5], 'img': book[6],
-                         'emoji': book[7], 'page_count': book[8]})
+                     'count': book[4], 'price': book[5], 'img': book[6],
+                     'emoji': book[7], 'page_count': book[8]})
     print(temp)
-    c.close()
     return temp
 
 
 def search_by_style(style):
+    conn = get_connection()
     c = conn.cursor()
     temp = []
     c.execute('''SELECT * FROM books
-                 WHERE style = (?)''', (style,))
+                 WHERE style = (?)''',(style,))
     all_book = c.fetchall()
     l = len(all_book)
     for i in range(l):
@@ -117,3 +104,5 @@ def search_by_style(style):
                      'emoji': all_book[i][7], 'page_count': all_book[i][8]})
     print(temp)
     return temp
+
+
